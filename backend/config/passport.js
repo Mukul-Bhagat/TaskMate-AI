6efed729b6dbca8071345ccd65b16f1dc2ea33b7
Mukul Bhagat <159694778+Mukul-Bhagat@users.noreply.github.com>
2025-10-01 10:09:30 +0530

    Update passport.js
    In strategy it was using wrong path

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -4,12 +4,11 @@ const User = require('../models/User');
 
 module.exports = function (passport) {
   passport.use(
-    new GoogleStrategy(
-      {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/api/auth/google/callback', // Must match Google Console URI
-      },
+    new GoogleStrategy({
+    clientID: process.env.GOOGLE_SIGNIN_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SIGNIN_CLIENT_SECRET,
+    callbackURL: '/api/auth/google/callback',
+  },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
           googleId: profile.id,
