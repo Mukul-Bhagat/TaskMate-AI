95bc8e0705d971788062bed398a4f91073c93332
Mukul Bhagat <159694778+Mukul-Bhagat@users.noreply.github.com>
2025-10-03 09:15:02 +0530

    Update User.js
    fix: make user password conditionally required

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,11 @@ const UserSchema = new mongoose.Schema(
 {
     name: {type: String, required: true},
     email: {type: String, required: true, unique:true},
-    password: {type: String, required: function() { return !this.googleId; }, },
+    password: {
+      type: String,
+      // Make password required only if the user does not have a googleId
+      required: function() { return !this.googleId; }, 
+    },
     profileImageUrl: {type: String, default: null},
     role: {type: String, enum: ["admin","member"], default:"member"},//Role-based access
 
